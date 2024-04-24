381038722f019e06e833512bf6f761bb3785d9e3
Isaias <chaisystem@gmail.com>
2024-04-24 00:04:57 -0600

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $useradmin=User::create([
+            'name' => 'Isaias',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin.')
+            ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
