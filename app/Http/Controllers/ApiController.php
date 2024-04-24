bc6e71d330c2c1ce97eb4305bd67c46e2fff0756
Isaias <chaisystem@gmail.com>
2024-04-24 17:49:40 -0600

    api-codigo

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($codigo)
+    {
+        if(Empleado::where('codigo',$codigo)->first()){
+                return '1';
+        }else{
+                return '0';
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
