b773d80113378e2c589bd44e3735c8a9c1cd5b83
Isaias <chaisystem@gmail.com>
2024-04-24 13:25:37 -0600

    list int

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -3,7 +3,43 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h2>{{$empleados}}</h2>
+        <table id="example" class="display" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nombre</th>
+                    <th>Salario en dólares</th>
+                    <th>Salario en pesos</th>
+                    <th>Correo</th>
+                    <th>Estatus</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($empleados as $empleado)
+                <tr>
+                    <td>{{ $empleado->codigo }}</td>
+                    <td>{{ $empleado->nombre }}</td>
+                    <td>{{ $empleado->salarioDolares }}</td>
+                    <td>{{ $empleado->salarioPesos }}</td>
+                    <td>{{ $empleado->correo }}</td>
+                    <td><input type="checkbox" class="statusCheckbox" data-empleado-id="{{ $empleado->id }}" {{ $empleado->estatus ? 'checked' : '' }}></td>
+                <td>
+                    <a href="{{ route('empleados.edit', ['empleado' => $empleado->id]) }}" class="text-blue-500 hover:text-blue-700 mr-2">Editar</a>
+                    <a href="{{ route('empleados.edit', ['empleado' => $empleado->id]) }}" class="text-red-500 hover:text-red-700 delete-btn" data-empleado-id="{{ $empleado->id }}">Borrar</a>
+                </td>
+
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/2.0.5/js/dataTables.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.0.5/css/dataTables.dataTables.min.css" rel="stylesheet" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
